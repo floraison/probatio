8cf4e45c672bfad5da85e36905f45221ee39c9b9
John Mettraux <jmettraux@gmail.com>
2024-12-18 17:26:41 +0900

    Scaffold Group and Test

--- lib/probatio.rb
+++ lib/probatio.rb
@@ -5,5 +5,42 @@
 module Probatio
 
   VERSION = '1.0.0'
+
+  class << self
+
+    def run(run_opts)
+
+      root_group = Group.new('_')
+
+      Dir[File.join(opts[:dir], '**', '*_test.rb')].each do |path|
+
+        read_file(root_group, path)
+      end
+
+      root_group.run(run_opts)
+    end
+
+    protected
+
+    def read_file(group, path)
+    end
+  end
+
+  class Group
+
+    def initialize(name, group_opts)
+
+      @name = name
+      @group_opts = group_opts
+
+      @children = []
+    end
+
+    def run(run_opts)
+    end
+  end
+
+  class Test
+  end
 end
 
