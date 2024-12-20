8740ae46fbd8dd2fb2f0019f41ddb273487a1e88
John Mettraux <jmettraux@gmail.com>
2024-12-20 14:32:04 +0900

    Give assertions and reporters their sections

--- lib/probatio.rb
+++ lib/probatio.rb
@@ -280,57 +280,15 @@ module Probatio
       instance_eval(&child.block)
 
     rescue AssertionError => aerr
+
+      # keeping calm and carrying on...
+
     ensure
+
       Probatio.despatch("#{child.type}_leave", self, child, run_opts)
     end
 
-    def assert(*as)
-
-      do_assert do
-
-        as.all? { |a| a == as[0] } ||
-        "no equal"
-      end
-    end
-
-    def assert_match(*as)
-
-      do_assert do
-
-        strings, others = as.partition { |a| a.is_a?(String) }
-        rex = others.find { |o| o.is_a?(Regexp) } || strings.pop
-
-        strings.all? { |s| s.match?(rex) } ||
-        "no match"
-      end
-    end
-
-    protected
-
-    def do_assert(&block)
-
-      r =
-        begin
-          block.call
-        rescue => err
-          err
-        end
-
-      if r.is_a?(StandardError) || r.is_a?(String)
-
-        Probatio.despatch(:test_fail, self, @__child, r)
-
-        fail AssertionError.new(r)
-
-      elsif r.is_a?(Exception)
-
-        raise r
-      end
-
-      Probatio.despatch(:test_succeed, self, @__child)
-
-      true # end on a positive note...
-    end
+    # assert_* methods belong to Probatio::Context but are defined below
   end
 
   class AssertionError < StandardError
@@ -374,6 +332,63 @@ module Probatio
     def depth; (@leaf || @group).depth rescue 0; end
   end
 end
+
+#
+# assertions
+
+class Probatio::Context
+
+  def assert(*as)
+
+    do_assert do
+
+      as.all? { |a| a == as[0] } ||
+      "no equal"
+    end
+  end
+
+  def assert_match(*as)
+
+    do_assert do
+
+      strings, others = as.partition { |a| a.is_a?(String) }
+      rex = others.find { |o| o.is_a?(Regexp) } || strings.pop
+
+      strings.all? { |s| s.match?(rex) } ||
+      "no match"
+    end
+  end
+
+  protected
+
+  def do_assert(&block)
+
+    r =
+      begin
+        block.call
+      rescue => err
+        err
+      end
+
+    if r.is_a?(StandardError) || r.is_a?(String)
+
+      Probatio.despatch(:test_fail, self, @__child, r)
+
+      fail Probatio::AssertionError.new(r)
+
+    elsif r.is_a?(Exception)
+
+      raise r
+    end
+
+    Probatio.despatch(:test_succeed, self, @__child)
+
+    true # end on a positive note...
+  end
+end
+
+#
+# reporters
 
 module Probatio::DotReporter
 
